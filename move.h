31cd844bf59a4646b4cc98a1c2e0aea8fefe8ce1
TheRebelCreeper <therebelcreeper@gmail.com>
2021-10-08 21:00:54 -0700

    Added correct data structure

--- move.h
+++ move.h
@@ -8,20 +8,6 @@
 
 #include "position.h"
 
-/*
-	There are special moves for kings and pawns only
-	For kings, 1 = O-O, 2 = O-O-O
-	For pawns, 4 = Q, 3 = R, 2 = B, 1 = N, 5 = EP
-*/
-typedef struct move
-{
-	int src;
-	int dst;
-	int piece;
-	int special;
-	int epSquare;
-} Move;
-
 int compareMoves(Move m1, Move m2);
 Move createMove(int piece, int src, int dst, int special, int epSquare);
 GameState playMove(GameState pos, Move move);
--- position.h
+++ position.h
@@ -13,16 +13,33 @@
 
 #define STARTING_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
+/*
+	There are special moves for kings and pawns only
+	For kings, 1 = O-O, 2 = O-O-O
+	For pawns, 4 = Q, 3 = R, 2 = B, 1 = N, 5 = EP
+*/
+typedef struct move
+{
+	int src;
+	int dst;
+	int piece;
+	int special;
+	int epSquare;
+} Move;
+
 typedef struct gameState
 {
 	U64 pieceBitboards[12];
 	U64 occupancies[3];
+	Move moveList[256];
 	int turn;
 	int castlingRights;
 	int enpassantSquare;
 	int halfMoveClock;
 	int fullMove;
 } GameState;
+
+
 
 extern GameState state;
 
