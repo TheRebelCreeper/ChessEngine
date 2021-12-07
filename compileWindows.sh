7faad724c9262ee680ffda801468a0860b7b9180
TheRebelCreeper <therebelcreeper@gmail.com>
2021-12-07 13:34:24 -0800

    Release mode!

--- compileWindows.sh
+++ compileWindows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DEFINES="-Drandom=rand -D__USE_MINGW_ANSI_STDIO=1 -DUSE_AVX2 -mavx2 -DUSE_SSE41 -msse4.1 -DUSE_SSSE3 -mssse3 -DUSE_SSE2 -msse2 -DUSE_SSE -msse"
-FLAGS="-Wall -g -fstrict-aliasing -fno-exceptions -Wno-unused-variable -Wno-unused-result -Wno-unused-but-set-variable -Wno-maybe-uninitialized -Ofast -fomit-frame-pointer"
+FLAGS="-Wall -fstrict-aliasing -fno-exceptions -Wno-unused-variable -Wno-unused-result -Wno-unused-but-set-variable -Wno-maybe-uninitialized -Ofast -fomit-frame-pointer"
 
 gcc *.c *.h ${FLAGS} ${DEFINES} -fopenmp -o Saxton.exe -static 
