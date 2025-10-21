f55967e8bf58fcf12f6d0e3448d75d7345146fcf
Aaron L <therebelcreeper@gmail.com>
2025-10-20 22:26:15 -0500

    Added tt to negamax but not qsearch (#36)
    * Added tt to negamax but not qsearch
    
    * Now sorts tt move

--- movepick.h
+++ movepick.h
@@ -3,6 +3,12 @@
 #include "movelist.h"
 #include "search.h"
 
+#define TT_HIT_SCORE		10000000
+#define KILLER_ONE			4500000
+#define KILLER_TWO			4400000
+#define HISTORY_SCORE_MAX	4200000
+#define HISTORY_SCORE_MIN	600
+
 void pick_move(MoveList *moves, int start_index);
 void score_moves(MoveList *moves, GameState *pos, Move tt_move, SearchInfo *info);
 
