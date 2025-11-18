01d5fb8874c2d89ec1c723fd2e6f2c2045cf7f4b
Aaron Lampert <TheRebelCreeper@gmail.com>
2025-11-18 21:58:28 +0000

    Super Basic Asp Window
    bench: 2579005

--- search.c
+++ search.c
@@ -368,6 +368,15 @@ void search_root(GameState *pos, SearchInfo *search_info)
 
         int new_score = search(alpha, beta, iterative_depth, pos, search_info);
 
+        if (new_score <= alpha || new_score >= beta) {
+            alpha = -INF;
+            beta = INF;
+            iterative_depth -= 1;
+            continue;
+        }
+        alpha = new_score - 50;
+        beta = new_score + 50;
+
         // If time is up, and we have completed at least depth 1 search, break out of loop
         if (!search_info->pv_table_length[0] || (search_info->stopped && iterative_depth > 1))
             break;
