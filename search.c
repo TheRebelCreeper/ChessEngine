7d46c69dd9c586e497bad4c406c4b72ae9cd218f
Aaron L <therebelcreeper@gmail.com>
2025-11-28 10:08:40 -0800

    doesn't drop into qsearch on reduction (#100)
    bench: 7821042

--- search.c
+++ search.c
@@ -273,7 +273,7 @@ int search(int alpha, int beta, int depth, GameState *pos, SearchInfo *info, boo
             r += !pv_node;
             r -= gives_check;
             r -= improving;
-            int reduced = CLAMP(new_depth - r, 0, new_depth);
+            int reduced = CLAMP(new_depth - r, 1, new_depth);
             score = -search(-alpha - 1, -alpha, reduced, &new_pos, info, true);
             if (score > alpha && reduced < new_depth) {
                 score = -search(-alpha - 1, -alpha, new_depth, &new_pos, info, !cut_node);
