ada88b92d3d8214545cf44d886dabb405ffcaefb
Aaron Lampert <therebelcreeper@gmail.com>
2025-12-04 01:14:37 -0600

    Prefetch in qsearch bench: 7627622

--- search.c
+++ search.c
@@ -441,6 +441,7 @@ int qsearch(int alpha, int beta, GameState *pos, SearchInfo *info, bool pv_node)
         if (!make_move(pos, &new_pos, current))
             continue;
 
+        prefetch_tt(new_pos.key);
         move_count++;
         info->ply++;
         int score = -qsearch(-beta, -alpha, &new_pos, info, pv_node);
